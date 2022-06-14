76e3e11c60c8fcf84eab12d2224f58f024c0e7f9
IsItHive <dbaishev97.secret@gmail.com>
2022-06-15 00:58:28 +0200

    Main change

--- mod.ts
+++ mod.ts
@@ -1,28 +1,24 @@
 export {
-    useFluent
-    } from './middleware.ts'
-export type {
-    FluentContextFlavor,
-    GrammyFluentOptions,
-    TranslateFunction,
-    LocaleNegotiator
+    useFluent,
+    type FluentContextFlavor,
+    type GrammyFluentOptions,
+    type TranslateFunction,
+    type LocaleNegotiator
 } from './middleware.ts'
 export {
     Fluent,
-    LoggingWarningHandler
-} from 'https://raw.githubusercontent.com/IsItHive/DenoFluently/main/mod.ts'
-export type {
-    AddTranslationOptions,
-    FluentBundleOptions,
-    GetTranslatorOptions,
-    LocaleId,
-    TranslationContext,
-    WarningHandler,
-    Warning,
-    BaseWarning,
-    TranslateWarning,
-    TranslateBundleMissingMessageWarning,
-    TranslateMessageMissingAttributeWarning,
-    TranslateMissingTranslationWarning,
-    LoggingWarningHandlerOptions
+    LoggingWarningHandler,
+    type AddTranslationOptions,
+    type FluentBundleOptions,
+    type GetTranslatorOptions,
+    type LocaleId,
+    type TranslationContext,
+    type WarningHandler,
+    type Warning,
+    type BaseWarning,
+    type TranslateWarning,
+    type TranslateBundleMissingMessageWarning,
+    type TranslateMessageMissingAttributeWarning,
+    type TranslateMissingTranslationWarning,
+    type LoggingWarningHandlerOptions
 } from 'https://raw.githubusercontent.com/IsItHive/DenoFluently/main/mod.ts'
